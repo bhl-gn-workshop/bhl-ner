1c49be06ff671c7c7d5281549a4d5219ab4593d5
Jocelyn Pender <jpend049@uottawa.ca>
2019-06-17 15:07:35 -0500

    copied code from cs_492_spring to clean repo and made changes to client

---
+++ README.md
@@ -0,0 +1,5 @@
+
+Start the GRPC client in a new terminal: 
+```
+python gRPC_client.py
+```
---
+++ gRPC_client.py
@@ -0,0 +1,40 @@
+import random
+import time
+import sys
+import os
+import grpc
+import protob_pb2
+import protob_pb2_grpc as bhl_grpc
+import math
+from visualization import Visualization
+
+
+#integration with GRPC
+class gRPCClient():
+    def __init__(self):
+        channel = grpc.insecure_channel('bhlrpc.globalnames.org:80')
+        self.stub = bhl_grpc.BHLIndexStub(channel)
+
+    def version(self):
+        return self.stub.Ver(protob_pb2.Void())
+    
+    def titles(self):
+        return self.stub.Titles(protob_pb2.TitlesOpt())
+    
+    def pages(self):
+        return self.stub.Pages(protob_pb2.PagesOpt(title_ids = [1, 2], with_text = True))
+
+def main():
+    client = gRPCClient()
+    version = client.version().value
+    print(version)
+    for page in client.pages():
+        #print(page.text)
+        for name in page.names:
+            print(name.value)
+    #res = client.query_stream(queryPages(page_count, number_of_pages))
+    
+
+    
+if __name__ == '__main__':
+    main()
